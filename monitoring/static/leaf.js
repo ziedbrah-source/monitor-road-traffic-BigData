4311b4a6832ee7c7698e5bf4fb2b22bf7b31e83e
Garrach <faresgarrach@insat.u-carthage.tn>
2023-05-10 20:53:04 +0100

    fixing map

--- monitoring/static/leaf.js
+++ monitoring/static/leaf.js
@@ -1,9 +1,9 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([36.8065, 10.1815], 13);
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
-    osm.addTo(map);
+    osm.addTo(mymap);
 
 // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
